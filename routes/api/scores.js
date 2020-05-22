5766645fbbb07ef7d01ce86f15bacf4ec007c026
Ira Herman <ira@xynetik.com>
2020-05-22 16:56:43 -0700

    Finished step 10

--- routes/api/scores.js
+++ routes/api/scores.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const scoresCtrl = require('../../controllers/scores');
+const scoresCtrl = require("../../controllers/scores");
 
-router.get('/', scoresCtrl.highScores);
-router.post('/', scoresCtrl.create);
+router.get("/", scoresCtrl.highScores);
+
+/*---------- Protected Routes ----------*/
+// Process the token for only the routes below
+router.use(require("../../config/auth"));
+router.post("/", scoresCtrl.create);
 
 module.exports = router;
