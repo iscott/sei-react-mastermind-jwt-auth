6891d5b9bd9221a3e5119b2a7a3db66faa30eb42
Ira Herman <ira@xynetik.com>
2020-05-22 16:48:22 -0700

    Finished step 9

--- src/utils/scoresService.js
+++ src/utils/scoresService.js
@@ -1,22 +1,25 @@
-const BASE_URL = '/api/scores/';
+import tokenService from "./tokenService";
+
+const BASE_URL = "/api/scores/";
 
 export default {
   index,
-  create
+  create,
 };
 
 function index() {
-  return fetch(BASE_URL).then(res => res.json());
+  return fetch(BASE_URL).then((res) => res.json());
 }
 
 function create(score) {
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-type': 'application/json'
+      "Content-type": "application/json",
+      // Add this header - don't forget the space after Bearer
+      Authorization: "Bearer " + tokenService.getToken(),
     },
-    body: JSON.stringify(score)
+    body: JSON.stringify(score),
   };
-  return fetch(BASE_URL, options).then(res => res.json());
+  return fetch(BASE_URL, options).then((res) => res.json());
 }
-
